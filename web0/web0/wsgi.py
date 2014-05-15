f2019d1e9ce0fe9c32df1939399f84ffd93653c0
das <das@dhcp2-212.si.umich.edu>
2014-05-15 17:14:03 -0400

    production settings included 2

--- web0/web0/wsgi.py
+++ web0/web0/wsgi.py
@@ -6,9 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
+import socket
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web0.settings")
+if socket.gethostname() == 'dallas':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web0.settings_production")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web0.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
