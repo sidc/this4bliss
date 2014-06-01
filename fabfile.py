28d1ee14b1572488683f59ff7b93d3c9fbbddc73
sidc <sidc@vps>
2014-06-01 16:01:49 -0700

    fab added

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import *
+
+# Hosts to deploy onto
+
+# Where your project code lives on the server
+env.project_root = '/home/sidc/this4bliss/web0'
+
+def deploy_static():
+    with cd(env.project_root):
+        run('source ../env/bin/activate')
+        run('python manage.py collectstatic -v0 --noinput')
