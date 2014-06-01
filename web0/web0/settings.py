485f3f5a83412558c1f80af14d8f5f7a74100ec5
das <das@dhcp2-212.si.umich.edu>
2014-06-01 19:07:09 -0400

    static root settings

--- web0/web0/settings.py
+++ web0/web0/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Template Context Processors to help django-suit http://django-suit.readthedocs.org/en/develop/
