47a6d0e30bd5d6821b89a1eb6381c655754c3216
das <das@dhcp2-212.si.umich.edu>
2014-06-02 12:54:12 -0400

    adding 500

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,26 @@
 from fabric.api import *
-
+import os
+from contextlib import contextmanager
 # Hosts to deploy onto
+VENV_DIR = '/home/sidc/this4bliss/env/'
+env.roledefs = {
+    'pro': ['sidc@this4bliss.com']
+} 
+env.project_root = '/home/sidc/this4bliss/web0'
 
 # Where your project code lives on the server
-env.project_root = '/home/sidc/this4bliss/web0'
+
+@contextmanager
+def source_virtualenv():
+    with prefix('source ' + os.path.join(VENV_DIR, 'bin/activate')):
+        yield
 
 def deploy_static():
     with cd(env.project_root):
-    	run("git pull")
-        run('python manage.py collectstatic -v0 --noinput')
-        run("touch web0.wsgi")
+        with source_virtualenv():
+            run("git pull")
+            run('python manage.py collectstatic -v0 --noinput')
+            run("touch web0.wsgi")
 
 def prepare_deploy():
     commit()
