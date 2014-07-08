5d74beb716b9268dbbf7bc8ebefc10eed1c3e0b1
das <das@dhcp2-212.si.umich.edu>
2014-07-08 16:27:31 -0400

    A lighter and discussed with Deva db.

--- web0/mobile/models.py
+++ web0/mobile/models.py
@@ -5,12 +5,11 @@ from django.db import models
 class Soul(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(null=True, blank=True, unique=True)
-    street = models.TextField(null=True, blank=True)
     city = models.TextField(null=True, blank=True)
-    state= models.TextField(null=True, blank=True)
+    state = models.TextField(null=True, blank=True)
     event = models.TextField(null=True, blank=True)
     date = models.DateField('distributed',null=True, blank=True)
-    book = models.CharField(max_length=400)
+    book = models.CharField(max_length=400, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     distributor = models.CharField(max_length=200)
 
